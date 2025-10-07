6c3f5ec831ab4ca2066ca28c81fffa14606b5af1
madhava.reddy@innosys.ai <madhava.reddy@innosys.ai>
2025-10-07 12:20:07 +0100

    build

--- src/worker.py
+++ src/worker.py
@@ -1,8 +1,15 @@
 from workers import WorkerEntrypoint, Response
-import os
-import uuid
-import time
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response("Hello World!")
+        prompt = PromptTemplate.from_template(
+            "Complete the following sentence: I am a {profession} and "
+        )
+        llm = OpenAI(api_key=self.env.API_KEY)
+        chain = prompt | llm
+
+        res = await chain.ainvoke({"profession": "electrician"})
+        return Response(res.split(".")[0].strip())
